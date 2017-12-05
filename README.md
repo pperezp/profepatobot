59817cf658e60ad5aa206ecf4ac839bb9c0d6402
pperezp <patodeath@gmail.com>
2017-12-05 18:35:47 -0300

    Se desarrolla más la base de datos. Se crean las tablas y se genera una consulta.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # profepatobot
-406813267:AAEvEQwnHNvwYXNuI0C2v9Ub9n6s7iRx_qA
 
 pip install python-telegram-bot --upgrade
 
--- model/bd/script.sql
+++ model/bd/script.sql
@@ -9,6 +9,88 @@ CREATE TABLE alumno(
   PRIMARY KEY (id)
 );
 
+CREATE TABLE tag (
+  id INT AUTO_INCREMENT,
+  nombre VARCHAR(200),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO tag VALUES(NULL, 'python');
+INSERT INTO tag VALUES(NULL, 'java');
+INSERT INTO tag VALUES(NULL, 'base de datos');
+INSERT INTO tag VALUES(NULL, '.net');
+INSERT INTO tag VALUES(NULL, 'java web');
+INSERT INTO tag VALUES(NULL, 'poo');
+INSERT INTO tag VALUES(NULL, 'asp');
+INSERT INTO tag VALUES(NULL, 'c#');
+INSERT INTO tag VALUES(NULL, 'sesiones');
+INSERT INTO tag VALUES(NULL, 'mssql');
+INSERT INTO tag VALUES(NULL, 'mysql');
+INSERT INTO tag VALUES(NULL, 'ajax');
+INSERT INTO tag VALUES(NULL, 'crud');
+
+CREATE TABLE link (
+  id INT AUTO_INCREMENT,
+  valor VARCHAR(2000),
+  PRIMARY KEY(id)
+);
+
+INSERT INTO link VALUES (null, 'https://www.youtube.com/watch?v=_B0niULnc5s'); /*POO*/
+INSERT INTO link VALUES (null, 'https://www.youtube.com/watch?v=ChXXB2jPwv8'); /*ASP y MS SQL y C#*/
+INSERT INTO link VALUES (null, 'https://www.youtube.com/watch?v=LG_ncb4VzSQ'); /*Java web y mysql*/
+INSERT INTO link VALUES (null, 'https://www.youtube.com/watch?v=qO510Nnp4JU'); /*c#, mssql, crud*/
+
+CREATE TABLE link_tag (
+  id INT AUTO_INCREMENT,
+  link INT,
+  tag INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(link) REFERENCES link(id),
+  FOREIGN KEY(tag) REFERENCES tag(id)
+);
+
+INSERT INTO link_tag VALUES (NULL, '1','6');
+INSERT INTO link_tag VALUES (NULL, '2','7');
+INSERT INTO link_tag VALUES (NULL, '2','10');
+INSERT INTO link_tag VALUES (NULL, '2','8');
+INSERT INTO link_tag VALUES (NULL, '3','5');
+INSERT INTO link_tag VALUES (NULL, '3','11');
+INSERT INTO link_tag VALUES (NULL, '4','8');
+INSERT INTO link_tag VALUES (NULL, '4','10');
+INSERT INTO link_tag VALUES (NULL, '4','13');
+
+/*Videos y sus tags*/
+SELECT
+  l.id AS 'ID link',
+  l.valor AS 'Link',
+  t.id AS 'ID tag',
+  t.nombre AS 'Tag'
+FROM
+  link_tag lt
+  INNER JOIN link l ON l.id = lt.link
+  INNER JOIN tag t ON t.id = lt.tag;
+
+
+/*Videos de un tag determinado*/
+SELECT
+  l.id AS 'ID link',
+  l.valor AS 'Link',
+  t.id AS 'ID tag',
+  t.nombre AS 'Tag'
+FROM
+  link_tag lt
+  INNER JOIN link l ON l.id = lt.link
+  INNER JOIN tag t ON t.id = lt.tag
+WHERE
+  t.nombre LIKE '%mssql%' OR
+  t.nombre LIKE '%java web%'
+  GROUP BY l.id;
+
+
+
+SELECT * FROM tag;
+SELECT * FROM link;
+SELECT * FROM link_tag;
 SELECT * FROM alumno;
 
 DROP DATABASE profe_pato_bot;
